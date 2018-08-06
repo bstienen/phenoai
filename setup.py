783f6a13016f10509b8a1e0e4562443d95e0ffa2
Bob Stienen <b.stienen@science.ru.nl>
2018-08-06 18:02:01 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name='phenoai',
-	version='0.1.2',
+	version='0.1.3',
 	author='Bob Stienen',
 	author_email='b.stienen@science.ru.nl',
 	description='Machine Learning interface for High Energy Physics Phenomenology',
