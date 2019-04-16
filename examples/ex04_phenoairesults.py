95de709d0122b47219117210c5559fdd5885ba61
Bob Stienen <b.stienen@science.ru.nl>
2019-04-16 16:07:01 +0200

    Renamed and fixed example 04

--- examples/ex04_phenoairesults.py
+++ examples/ex04_phenoairesults.py
@@ -13,7 +13,7 @@ X = np.random.rand(1000,3)*10-5
 
 # Create PhenoAI instance and load AInalysis
 
-from phenoai.phenoai import PhenoAI
+from phenoai import PhenoAI
 from phenoai import logger
 
 logger.to_stream(0)
